f515508658b4ea96b3bae534b84f1fed4d80a6f8
Matheus Jurkovich <matheusjurkovich@gmail.com>
2022-05-04 22:40:11 -0300

    added scrollreveal

--- script.js
+++ script.js
@@ -2,3 +2,19 @@ var btn = document.querySelector('#back-to-top')
 btn.addEventListener('click', function () {
   window.scrollTo(0, 0)
 })
+
+ScrollReveal({
+  origin: 'bottom',
+  distance: '30px',
+  duration: 700
+}).reveal(`
+  #cultura,
+  #cultura #principal img,
+  #secundaria,
+  #secundaria iframe,
+  #cultivo,
+  #cultivo p,
+  #contatos,
+  .zap,
+  #integrantes,
+  .unip`)
